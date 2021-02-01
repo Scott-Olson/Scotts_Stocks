83daa48a4f8f7daeb2e890cc705464ab0d5bc5ca
scott-olson <scotteolson3@gmail.com>
2021-02-01 12:27:02 -0800

    add secrets and keys to populate

--- populate_db.py
+++ populate_db.py
@@ -1,9 +1,12 @@
 import sqlite3
+import secrets
+
+API_KEY = secrets.api_key
+SECRET_KEY = secrets.secret_key
+ENDPOINT = secrets.endpoint
 
 connection = sqlite3.connect('app.db')
 
 cursor = connection.cursor()
-
-cursor.execute("DELETE FROM stock")
 
 connection.commit()
