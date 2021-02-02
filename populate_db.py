7154640ef35bf92865c95f4906d5f9939d6863f1
scott-olson <scotteolson3@gmail.com>
2021-02-01 17:39:46 -0800

    add symbol retrieval function

--- populate_db.py
+++ populate_db.py
@@ -12,7 +12,9 @@ ENDPOINT = secrets.endpoint
 
 # set up databse connection
 connection = sqlite3.connect('app.db')
+connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
+
 
 # instantiate trade api
 api = tradeapi.REST(API_KEY, SECRET_KEY, base_url = ENDPOINT)
@@ -23,8 +25,39 @@ assets = api.list_assets()
 print(sys.getsizeof(assets) / 1024)
 print(len(assets))
 
+def fetch_current_symbols(cursor):
+    # fetch the rows from the db
+    cursor.execute("""
+        SELECT symbol, company FROM stock
+    """)
+    rows = cursor.fetchall()
+    # return all the symbols
+    symbols = [row['symbol'] for row in rows]
+
+    return symbols
+
+
+# retrieve the current symbols stored in the db
+symbols = fetch_current_symbols(cursor)
+
+for asset in assets:
+    # structure the query and params as would be expected by SQLite3
+    query = "INSERT INTO stock(symbol, company) VALUES (?, ?)"
+    params = (asset.symbol, asset.name)
+    try:
+        # constrain the try to active and tradable stocks
+        if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
+            # execute the query
+            print(f"Inserting {asset.name} into db as {asset.symbol}.")
+            cursor.execute(query, params)
+
+    except Exception as e:
+        print(f"Symbol: {asset.symbol}, Comapny: {asset.name}")
+        print(e)
 
 
 
-
+# save changes to the db
 connection.commit()
+
+
